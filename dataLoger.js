da522d8f070e32c20c4b22d9bd7e490ff865cd20
Stachugit <stachugb123@gmail.com>
2024-10-26 14:53:24 +0200

    obsluga inputow drukujemy na konsolce

--- dataLoger.js
+++ dataLoger.js
@@ -5,3 +5,27 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+function askQuestion(query) {
+    return new Promise(resolve => rl.question(query, resolve));
+  }
+  
+  async function main() {
+    try {
+      const firstName = await askQuestion("Podaj imię: ");
+      const lastName = await askQuestion("Podaj nazwisko: ");
+      const age = await askQuestion("Podaj wiek: ");
+  
+      const userData = {
+        firstName,
+        lastName,
+        age
+      };
+      console.log("Dane użytkownika:", userData);
+      rl.close();
+    } catch (error) {
+      console.error("Wystąpił błąd:", error);
+      rl.close();
+    }
+  }
+main();
