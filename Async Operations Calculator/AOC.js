26bf7d582ea25425b7bdbf7ad089f2edcf08dcf3
Stachugit <stachugb123@gmail.com>
2024-10-27 16:55:11 +0100

    funkcja do inputa

---
+++ Async Operations Calculator/AOC.js
@@ -0,0 +1,10 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function getUserInput(question) {
+    return new Promise((resolve) => rl.question(question, resolve));
+}
