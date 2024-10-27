15c5f59da5b62920a726857b8cb704850c9231bd
Stachugit <stachugb123@gmail.com>
2024-10-27 17:26:34 +0100

    inputy

--- Dzejson data manadzer/mngr.js
+++ Dzejson data manadzer/mngr.js
@@ -9,3 +9,22 @@ const rl = readline.createInterface({
 function promptUser(query) {
     return new Promise(resolve => rl.question(query, resolve));
 }
+
+async function addObject() {
+    const name = await promptUser('Podaj imie ');
+    const age = await promptUser('Podaj wiek ');
+    const email = await promptUser('Podaj email ');
+    const filePath = await promptUser('Podaj ściezke do pliku Dżejson: ');
+
+    const newObject = { name, age, email };
+
+    let data = [];
+    if (fs.existsSync(filePath)) {
+        const fileContent = fs.readFileSync(filePath);
+        data = JSON.parse(fileContent);
+    }
+
+    data.push(newObject);
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+    console.log('Dane dodane pomyślnie');
+}
